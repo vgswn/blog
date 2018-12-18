188110e073aa23234ae970e149e02c893045117c
= <iit2015038@iiita.ac.in>
2018-12-19 03:57:00 +0530

    g

--- db/schema.rb
+++ db/schema.rb
@@ -41,10 +41,5 @@ ActiveRecord::Schema.define(version: 2018_12_18_052517) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-  #create_table "msg", force: :cascade do |t|
-   # t.string "name",default:"",null:false
-    #t.string "email",default:"",null:false
-    #t.string "message",default:"",null:false
-  #end
 
 end
